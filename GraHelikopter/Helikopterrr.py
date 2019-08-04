40f72d8512071acf6c7cc0b3369dddc97ccf5c72
SebastianTyr <48628456+SebastianTyr@users.noreply.github.com>
2019-08-04 22:16:20 +0200

    Wersja testowa gry

---
+++ GraHelikopter/Helikopterrr.py
@@ -0,0 +1,101 @@
+import pygame
+import os
+import random
+
+pygame.init()
+w = 600
+h = 600
+screen = pygame.display.set_mode((w,h))
+
+def write(text, size):
+    font = pygame.font.SysFont("Arial", size)
+    rend = font.render(text, 0, (255,100,100))
+    x = (w - rend.get_rect().width)/2
+    y = (h - rend.get_rect().height)/2
+    screen.blit(rend, (x,y))
+    
+_display = "menu"
+
+class Obstacle():
+    def __init__(self, x, width):
+        self.x = x
+        self.width = width
+        self.y_up = 0
+        self.height_up = random.randint(150,250)
+        self.space = 300
+        self.y_down = self.height_up + self.space
+        self.height_down = h - self.y_down
+        self.color = (160,140,190)
+        self.form_up = pygame.Rect(self.x, self.y_up, self.width, self.height_up)
+        self.form_down = pygame.Rect(self.x, self.y_down, self.width, self.height_down)
+    def draw(self):
+        pygame.draw.rect(screen, self.color, self.form_up, 0)
+        pygame.draw.rect(screen, self.color, self.form_down, 0)
+    def move(self, v):
+        self.x = self.x - v
+        self.form_up = pygame.Rect(self.x, self.y_up, self.width, self.height_up)
+        self.form_down = pygame.Rect(self.x, self.y_down, self.width, self.height_down)
+    def collision(self, player):
+        if self.form_up.colliderect(player) or self.form_down.colliderect(player):
+            return True
+        else:
+            return False
+        
+class Helicopter():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.width = 30
+        self.height = 50
+        self.form = pygame.Rect(self.x, self.y, self.width, self.height)
+        self.graph = pygame.image.load(os.path.join('helicoptrr.png'))
+    def draw(self):
+        screen.blit(self.graph, (self.x, self.y))
+    def move(self ,v):
+        self.y = self.y + v
+obstacle = []
+for i in range(21):
+    obstacle.append(Obstacle(i*w/20, w/20))
+    
+game = Helicopter(250,250)
+dy = 0
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                dy = -0.5
+            elif event.key == pygame.K_DOWN:
+                dy = 0.5
+            if event.key == pygame.K_SPACE:
+                if _display != "play":
+                    player = Helicopter(250,250)
+                    dy = 0
+                    _display = "play"                    
+    screen.fill((0,0,0))
+    
+    if _display == "menu":
+        write("Naciśnij spację, aby zacząć",40)
+        logo = pygame.image.load(os.path.join('logo.png'))
+        screen.blit(logo, (80,30))
+    elif _display == "play":
+        for o in obstacle:
+            o.move(0.5)
+            o.draw()
+            if o.collision(game.form):
+                _display = "end"
+        for o in obstacle:
+            if o.x <= -o.width:
+                obstacle.remove(o)
+                obstacle.append(Obstacle(w, w/20))
+        game.draw()
+        game.move(dy)
+    elif _display == "end":
+        logo = pygame.image.load(os.path.join('logo.png'))
+        screen.blit(logo, (80,30))
+        write("Niestety przegrywasz", 50, 290, 20)
+        
+    pygame.display.update()
